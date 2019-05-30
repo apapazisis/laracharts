e4c138ea820642a4a1ae6b22f146b9ebad49a42e
Apostolos Papazisis <apapazisis@users.noreply.github.com>
2019-05-31 01:24:15 +0200

    Update EchartsServiceProvider.php

--- src/EchartsServiceProvider.php
+++ src/EchartsServiceProvider.php
@@ -8,11 +8,11 @@ class EchartsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/Views', 'echarts');
+        $this->loadViewsFrom(__DIR__.'/Views', 'echarts');
 
         $this->publishes([
-            __DIR__ . '/js/worker.js' => public_path('vendor/charts'),
-            __DIR__.'/js/charts.js'   => public_path('vendor/charts')
+            __DIR__.'/js/worker.js' => public_path('vendor/charts'),
+            __DIR__.'/js/charts.js' => public_path('vendor/charts')
         ], 'public');
     }
 }
