f479dd4c4319d322611766a811f316df9e3943d1
Apostolos Papazisis <apapazisis@users.noreply.github.com>
2019-05-31 02:00:39 +0200

    Update charts.js

--- src/js/charts.js
+++ src/js/charts.js
@@ -3,7 +3,7 @@ function convertFunctions(chartArgs)
     Object.keys(chartArgs).forEach((key) => {
         if (typeof chartArgs[key] === "string") {
             if (chartArgs[key].includes("function")) {
-                chartArgs[key] = new Function("return " + chartArgs[key])()
+                chartArgs[key] = new Function("return " + chartArgs[key])();
             }
         }
 
